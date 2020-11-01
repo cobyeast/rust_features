c725b57e8b0a229b5224965985b356501bdea6e7
cobyeast <cobyeastwood@gmail.com>
2020-10-31 18:09:20 -0700

    hashmaps

---
+++ src/hashmaps.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap;
+
+fn hash() {
+  let mut shapes = HashMap::new();
+
+  shapes.insert(String::from("triangle"), 3);
+  shapes.insert(String::from("square"), 4);
+
+  shapes.insert("square".into(), 5);
+  shapes.entry("circle".into()).or_insert(1);
+
+  {
+    // New Scope For Borrowing Value
+    let actual = shapes.entry("circle".into()).or_insert(3); // Reference
+    *actual = 0; // Dereference
+  }
+
+  for (key, val) in shapes {
+    print!("{}: {}\n", key, val);
+  }
+}
+
+pub fn hashmaps() {
+  hash();
+}
