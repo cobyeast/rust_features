2c6ef398ed35e1dbb32dbcccb9696e7e0a8c843c
cobyeast <cobyeastwood@gmail.com>
2020-11-14 13:25:34 -0800

    cirular references

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #[allow(dead_code)]
 #[allow(unused_imports)]
 #[allow(unused_variables)]
-mod reference_count_atomic;
+mod cicular_reference;
 
 fn main() {
-    reference_count_atomic::mutex();
+    cicular_reference::cicular();
 }
